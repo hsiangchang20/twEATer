9aee0cf7280e6343a31f12e64321b007e59da117
Chenyungchin <b08901061@ntu.edu.tw>
2021-01-07 18:56:34 +0800

    change class to function

--- src/containers/MainPage/Restaurants/Restaurant.js
+++ src/containers/MainPage/Restaurants/Restaurant.js
@@ -1,6 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Restaurant extends Component {
+export default function Restaurant(props) {
+    const restaurantIDs = ["1", "2", "3", "4", "5", "6", "7"];
+    const { id } = props.match.params;
+    return id && restaurantIDs.includes(id) ? (
+        <article>
+            <h1>Restaurant #{id}</h1>
+            <p>This is the {id}-th restaurant</p>
+        </article>
+    ) : (
+            <div>
+                <h3>Error: Restaurant #{id} NOT FOUND</h3>
+            </div>
+        )
+}
+/*
+{
     render() {
         const restaurantIDs = ["1", "2", "3", "4", "5", "6", "7"];
         const { id } = this.props.match.params;
@@ -16,3 +31,4 @@ export default class Restaurant extends Component {
             )
     }
 }
+*/
--- src/containers/MainPage/Searches/Searches.js
+++ src/containers/MainPage/Searches/Searches.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+export default function Searches() {
+    const RestaurantIDs = ["1", "2", "3", "4", "5", "6", "7"];
+    const restaurants_list = RestaurantIDs.map((i, index) => (
+        <li key={index}>
+            <NavLink to={"/restaurant/" + i}>
+                Restaurant #{i}
+            </NavLink>
+        </li>
+    ));
+    return (
+        <div>
+            <h3>Search Results ---</h3>
+            {restaurants_list}
+        </div>
+    );
+}
+/*
 export default class Searches extends Component {
     render() {
         const RestaurantIDs = ["1", "2", "3", "4", "5", "6", "7"];
@@ -19,3 +36,4 @@ export default class Searches extends Component {
         );
     }
 }
+*/
