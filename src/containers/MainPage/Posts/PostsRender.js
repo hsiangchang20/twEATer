7125d70e695c9ea2d55c39f73f73653c1a33ab6f
Chenyungchin <b08901061@ntu.edu.tw>
2021-01-07 19:02:28 +0800

    change all classes to functions

--- src/containers/MainPage/Posts/PostsRender.js
+++ src/containers/MainPage/Posts/PostsRender.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import Post from "../../../components/Post/Post";
 
+
+export default function PostRender(props) {
+    const postIDs = ["1", "2", "3", "4", "5", "6", "7"];
+    const { id } = props.match.params;
+    return id && postIDs.includes(id) ? (
+        <Post id={id} />
+    ) : (
+            <div>
+                <h3>Error: Post #{id} NOT FOUND</h3>
+            </div>
+        )    
+}
+/*
 export default class PostRender extends Component {
     render() {
         const postIDs = ["1", "2", "3", "4", "5", "6", "7"];
@@ -14,3 +27,4 @@ export default class PostRender extends Component {
             )
     }
 }
+*/
