9d536bfc33cbb34636873bf49664f0c63c76ff4d
hsiang20 <h511168@nehs.hc.edu.tw>
2021-01-19 02:35:52 +0800

    roulette

--- src/containers/MainPage/Roulette/Roulette.js
+++ src/containers/MainPage/Roulette/Roulette.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import './css/roulette.css'
 import rouletteImg from './image/roulette.png'
 
 export default function Roulette() {
-    
+    // -webkit-animation:spin 1s linear infinite; -moz-animation:spin 1s linear infinite; animation:spin 1s linear infinite;
+    const [animation, setAnimation] = useState({})
+    const [restaurant, SetRestaurant] = useState('')
+
+    function spinAnimate() {
+        setAnimation({"WebkitAnimation":"spin 1s linear infinite", "MozAnimation":"spin 1s linear infinite", "animation":"spin 1s linear infinite"})
+        window.setTimeout(( () => {
+            setAnimation({});
+            chooseRestaurant();
+        }), 3000);
+    }
+
+    function chooseRestaurant(){
+        SetRestaurant('五福餃子')
+    }
+
     return (
-        <div>
-            <div className="rotateImg">
+        <div className="roulette-wrapper">
+            <div className="btn-wrapper">
+                <button className="btn-roulette" onClick={spinAnimate}>I'M FEELING LUCKY</button>
+            </div>
+            <div className="rotateImg" style={animation}>
                 <img src={rouletteImg} />
+            </div>
+            <div className="result-wrapper">
+                <button className="result">{restaurant}</button>
             </div>
         </div>
     )
