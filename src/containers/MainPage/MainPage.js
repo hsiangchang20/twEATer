c7b84e8b0dd0a252381fbd6733e1cb9e149c80b3
hsiang20 <h511168@nehs.hc.edu.tw>
2021-01-20 04:49:52 +0800

    some decoration

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from "react";
 import { NavLink, Switch, Route, Redirect } from "react-router-dom";
 //import ScriptTag from 'react-script-tag'
 import './mainpage.css'
-
+import logo from "../../components/Images/logo-reverse.png"
+import logoTitle from "../../components/Images/tweater-reverse.png"
 import Posts from "./Posts/Posts";
 import Searches from "./Searches/Searches";
 import PostsRender from "./Posts/PostsRender";
@@ -12,51 +13,75 @@ import LoginPage from "./Login/loginPage";
 import Profile from "./Profile/Profile";
 import Add from "./Add/Add"
 
-export default function MainPage() {
+export default function MainPage(props) {
 
     const [login, setLogin] = useState(false);
     const [userdata, setUserdata] = useState([]);
+    const [postStyle, setPostStyle] = useState({background: '#f4ff53'});
+    const [searchStyle, setSearchStyle] = useState({});
+    const [rouletteStyle, setRouletteStyle] = useState({});
+    const [addStyle, setAddStyle] = useState({});
+    const [profileStyle, setProfileStyle] = useState({});
     console.log(userdata._id);
+
+    function setMenuColor(page) {
+        setPostStyle({});
+        setSearchStyle({});
+        setRouletteStyle({});
+        setAddStyle({});
+        setProfileStyle({});
+        if (page === 'post') {setPostStyle({background: '#f4ff53'})}
+        else if (page === 'search') {setSearchStyle({background: '#f4ff53'})}
+        else if (page === 'roulette') {setRouletteStyle({background: '#f4ff53'})}
+        else if (page === 'add') {setAddStyle({background: '#f4ff53'})}
+        else if (page === 'profile') {setProfileStyle({background: '#f4ff53'})}
+        
+    }
+
     return (
         <div>
             <title>Login V1</title>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <div className="limiter">
-                <div className="container-background100">
-                    {login?
-                        (<div>
-                            <div id='menu-wrapper'>
-                                <div id='menu'>
-                                    <ul>
-                                        <li>
-                                            <button className="main-button">
-                                                <NavLink to={"/post/" + userdata._id} className="main-button-text">Post</NavLink>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="main-button">
-                                                <NavLink to={"/search/" + userdata._id} className="main-button-text">Search</NavLink>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="main-button">
-                                                <NavLink to={"/roulette/" + userdata._id} className="main-button-text">Roulette</NavLink>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="main-button">
-                                                <NavLink to={"/add/" + userdata._id} className="main-button-text">Add</NavLink>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="main-button">
-                                                <NavLink to={"/profile/" + userdata._id} className="main-button-text">Profile</NavLink>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
+                {login?
+                    (<div>
+                        <div id='menu-wrapper'>
+                            <div id='menu'>
+                                <ul>
+                                    <li>
+                                        <img src={logo} />
+                                        <img src={logoTitle} />
+                                    </li>
+                                    <li className="main-button-li" style={postStyle}>
+                                        <button className="main-button" onClick={()=>{setMenuColor("post")}}>
+                                            <NavLink to={"/post/" + userdata._id} className="main-button-text">Post</NavLink>
+                                        </button>
+                                    </li>
+                                    <li className="main-button-li" style={searchStyle}>
+                                        <button className="main-button" onClick={()=>{setMenuColor("search")}}>
+                                            <NavLink to={"/search/" + userdata._id} className="main-button-text">Search</NavLink>
+                                        </button>
+                                    </li>
+                                    <li className="main-button-li" style={rouletteStyle}>
+                                        <button className="main-button" onClick={()=>{setMenuColor("roulette")}}>
+                                            <NavLink to={"/roulette/" + userdata._id} className="main-button-text">Roulette</NavLink>
+                                        </button>
+                                    </li>
+                                    <li className="main-button-li" style={addStyle}>
+                                        <button className="main-button" onClick={()=>{setMenuColor("add")}}>
+                                            <NavLink to={"/add/" + userdata._id} className="main-button-text">Add</NavLink>
+                                        </button>
+                                    </li>
+                                    <li className="main-button-li" style={profileStyle}>
+                                        <button className="main-button" onClick={()=>{setMenuColor("profile")}}>
+                                            <NavLink to={"/profile/" + userdata._id} className="main-button-text">Profile</NavLink>
+                                        </button>
+                                    </li>
+                                </ul>
                             </div>
+                        </div>
+                        <div className="container-background100">
                             <div id='wrapper'>
                                 <Switch>
                                     <Route path="/post/:userid?" component={Posts} />
@@ -70,7 +95,8 @@ export default function MainPage() {
                                 </Switch>
                             </div>
                         </div>
-                        ) : (
+                    </div>) : (
+                        <div className="container-background100-login">
                             <LoginPage onClick={(data) => {
                                 if (data !== undefined) {
                                     setLogin(true);
@@ -78,9 +104,9 @@ export default function MainPage() {
                                 }
                                 //else console.log(data);
                             }} />
-                        )
-                    }
-                </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
