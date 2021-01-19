aeae802d0c6cf66fc21ee4f543cb7828df5552f1
yuhsinchan <b08901046@g.ntu.edu.tw>
2021-01-20 00:43:07 +0800

    daodaodao

--- src/containers/MainPage/Restaurants/Restaurant.js
+++ src/containers/MainPage/Restaurants/Restaurant.js
@@ -6,26 +6,27 @@ import { useQuery, useLazyQuery } from "@apollo/client";
 export default function Restaurant(props) {
     //const restaurantIDs = ["1", "2", "3", "4", "5", "6", "7"];
     const { name } = props.match.params;
-    console.log(props)
     const { loading, error, data} = useQuery(RESTAURANT_QUERY, {variables: {name: name}});
     const [rest, {loading2, data2}] = useLazyQuery(RESTAURANT_QUERY)
     const [restaurant, setRestaurant] = useState('dao')
     const [photo, setPhoto] = useState('')
-    useEffect(()=>{
-        console.log(data)
-        
-        if (data){
+    useEffect(()=>{    
+        console.log(data)    
+        if (data && (data.restaurant[0])){
             setRestaurant(data.restaurant[0]);
-            setPhoto(data.restaurant[0].posts[0].photo)
+            if(data.restaurant[0].posts[0]!==undefined){
+                setPhoto(data.restaurant[0].posts[0].photo)
+                console.log(data.restaurant[0].posts[0].photo)
+            }
         }
     })
 
-    const restaurant_view = (restaurant === undefined) ? (<div className="name"><p>Restaurant does not exist</p></div>) : (
+    const restaurant_view = (restaurant === 'dao') ? (<div className="name"><p>Restaurant does not exist</p></div>) : (
         <div className="wrap-restaurant">
             <div className="name">
                 <p>{'< ' + restaurant.name + ' >'}</p>
             </div>
-            <img src={photo}></img>
+            {photo!==""?<img src={photo}></img>:""}
             <div className="info">
                 <p>Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp; {restaurant.type}</p>
                 <p>Telephone&ensp;&nbsp;:&nbsp;&nbsp; {restaurant.tele}</p>
--- src/graphql/user_query.js
+++ src/graphql/user_query.js
@@ -16,5 +16,5 @@ export const USER_QUERY = gql`
         body
         }
     }
-  }
+  } 
 `
